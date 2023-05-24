54e62653d2e99f765004e3a5c692283be8d0d391
Merlijnos <99071140@mydavinci.nl>
2023-05-24 10:12:10 +0200

    buttons 2

--- M02.O2/buttons.js
+++ M02.O2/buttons.js
@@ -2,10 +2,19 @@ let count = [0, 0, 0];
 let buttonNumber = 0;
 
 function handleButtonClick(btnIndex) {
-  buttonNumber = btnIndex;
-  count[btnIndex-1]++;
-  document.getElementById(`button${btnIndex}`).innerHTML = count[btnIndex-1];
-  document.getElementById(`button${btnIndex}Count`).innerHTML = count[btnIndex-1];
-  document.getElementById("img").src = `Images/bg${buttonNumber}.jpg`;
-  document.getElementById("corner").src = `Images/${buttonNumber}.jpg`;
+  if (buttonNumber !== btnIndex) {
+    buttonNumber = btnIndex;
+    count[btnIndex-1]++;
+    document.getElementById(`button${btnIndex}Count`).innerHTML = count[btnIndex-1];
+    document.getElementById("img").src = `Images/bg${buttonNumber}.jpg`;
+    document.getElementById("corner").src = `Images/${buttonNumber}.jpg`;
+  }
+  document.getElementById(`btn${btnIndex}`).style.backgroundColor = "red";
+  document.getElementById(`btn${btnIndex}`).disabled = true;
+  for (let i = 1; i <= 3; i++) {
+    if (i !== btnIndex) {
+      document.getElementById(`btn${i}`).style.backgroundColor = "green";
+      document.getElementById(`btn${i}`).disabled = false;
+    }
+  }
 }
